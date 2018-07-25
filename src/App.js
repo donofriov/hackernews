4ba43c69e63af70fa7b4ea097b5d24bd9d254ba3
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2018-07-25 07:27:06 -0400

    add more results, caching, and tests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
-const DEFAULT_QUERY = 'javascript';
+const DEFAULT_QUERY = '';
+const DEFAULT_HPP = '5';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}`;
 
 // ES5
 // function isSearched(searchTerm) {
@@ -18,11 +22,12 @@ const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 // }
 //
 // ES6
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+// const isSearched = searchTerm => item =>
+//   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 // ES6 Class Component
 class App extends Component {
+  _isMounted = false;
   // The constructor instantiates the class with all its properties.
   // The business logic of class methods should be defined outside
   // of the constructor and then bound to the constructor.
@@ -30,23 +35,32 @@ class App extends Component {
     super(props);
 
     this.state = {
-      result: null,
+      results: null,
+      searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     }
 
     // Binds class method to class constructor
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
+    this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
   };
 
   onDismiss(id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
     const isNotId = item => item.objectID !== id;
     // You can evaluate each item in the list based on a filter condition.
     // If the evaluation for an item is true, the item stays in the list.
     // Otherwise it will be filtered from the list.
-    const updatedHits = this.state.result.hits.filter(isNotId);
+    const updatedHits = hits.filter(isNotId);
     this.setState({
-      result: {...this.state.result, hits: updatedHits }
+      results: {...results, [searchKey]: { hits: updatedHits, page }}
     });
   }
 
@@ -54,28 +68,75 @@ class App extends Component {
     this.setState({ searchTerm: event.target.value });
   }
 
-  async componentDidMount() {
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+    event.preventDefault();
+  }
+
+  async fetchSearchTopStories(searchTerm, page = 0) {
     this.setState({ isLoading: true });
-
     try {
-      const result = await axios.get(url);
-
-      this.setState({
-        result: result.data,
-        isLoading: false
-      })
-
+      const result = await axios.get(`${url}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`);
+      this._isMounted && this.setSearchTopStories(result.data, false);
     } catch (error) {
-      this.setState({
+      this._isMounted && this.setState({
         error,
         isLoading: false
       });
     }
   }
 
+  setSearchTopStories(result, isLoading) {
+    // get the hits and page from the result
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+
+    // check if there are already old hits
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : [];
+
+    // merge old and new hits together
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    // set the merged hits and page in the local component state
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page, isLoading }
+      }
+    });
+  }
+
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
   render() {
     // Destructure local state object
-    const { searchTerm, result } = this.state;
+    const { searchTerm, results, searchKey, error } = this.state;
+    const page = (results && results[searchKey] && results[searchKey].page) || 0;
+    const list = (results && results[searchKey] && results[searchKey].hits) || [];
+
     return (
       <div className="page">
         <h2 className="App">Hacker News</h2>
@@ -83,17 +144,25 @@ class App extends Component {
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
             Search
           </Search>
         </div>
-        { result &&
-          <Table
-            list={result.hits}
-            pattern={searchTerm}
+        { error
+          ? <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+          : <Table
+            list={list}
             onDismiss={this.onDismiss}
           />
         }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
@@ -102,9 +171,9 @@ class App extends Component {
 // Functional Stateless Component
 // The best practice is to use ES6 destructuring in the
 // function signature to destructure the props.
-const Search = ({ value, onChange, children }) =>
-    <form>
-      {children} <input
+const Search = ({ value, onChange, onSubmit, children }) =>
+    <form onSubmit={onSubmit}>
+      <input
         type="text"
         // Forces this to be a controlled component
         // The unidirectional data flow loop for the input field is self-contained now.
@@ -112,18 +181,21 @@ const Search = ({ value, onChange, children }) =>
         value={value}
         onChange={onChange}
       />
+      <button type="submit">
+        {children}
+      </button>
     </form>
 
 // Functional Stateless Component
 // Enforce only to have props as input and JSX as output
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({ list, onDismiss }) =>
   <div className="table">
     {/* You can remove the block body, meaning the curly braces, of the ES6 arrow function.
         In a concise body, an implicit return is attached.
 
         list and searchTerm can now be used without this.state because it has been destructured.
     */}
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
@@ -144,7 +216,7 @@ const Table = ({ list, pattern, onDismiss }) =>
   </div>
 
 // Functional Stateless Component
-const Button = ({ onClick, className = '', children }) =>
+const Button = ({ onClick, className, children }) =>
   <button
     onClick={onClick}
     className={className}
@@ -153,4 +225,40 @@ const Button = ({ onClick, className = '', children }) =>
     {children}
   </button>
 
+Button.defaultProps = {
+  className: '',
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired,
+};
+
+Search.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+}
+
 export default App;
+
+export {
+  Button,
+  Search,
+  Table
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,115 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App, { Search, Button, Table } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// • Always begin with a shallow test
+// • If componentDidMount() or componentDidUpdate() should be tested, use mount()
+// • If you want to test component lifecycle and children behavior, use mount()
+// • If you want to test a component’s children rendering with less overhead than mount() and you
+//   are not interested in lifecycle methods, use render()
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <App />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Search', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <Search
+        onChange={() => 'I was changed'}
+        onSubmit={() => 'I was submitted'}
+      >
+        Search
+      </Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Search
+        onChange={() => 'I was changed'}
+        onSubmit={() => 'I was submitted'}
+      >
+        Search
+      </Search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Button', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button onClick={() => 'I was clicked'}>Give Me More</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Button onClick={() => 'I was clicked'}>Give Me More</Button>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+
+  it('simulates click events', () => {
+    const mockCallBack = jest.fn();
+    const element = shallow(
+      <Button onClick={mockCallBack}>More</Button>
+    );
+
+    element.find('button').simulate('click');
+
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+});
+
+describe('Table', () => {
+  const props = {
+    onDismiss: () => 'I was clicked',
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+    ],
+  };
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table { ...props } />, div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Table { ...props } />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table { ...props } />
+    )
+
+    expect(element.find('.table-row').length).toBe(2);
+  });
 });
